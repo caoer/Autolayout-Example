bb6a1e43e8ca32b4cc694fb569ebbb8db3e872c9
Zitao Xiong <caoer115@gmail.com>
2014-11-14 14:21:48 -0500

    init

---
+++ autolayoutdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autolayoutdemo
+//
+//  Created by Zitao Xiong on 14/11/2014.
+//  Copyright (c) 2014 nanaimostudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autolayoutdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autolayoutdemo
+//
+//  Created by Zitao Xiong on 14/11/2014.
+//  Copyright (c) 2014 nanaimostudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
